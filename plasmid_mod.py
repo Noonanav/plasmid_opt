a6c301bb2c53c2b8ab9a2963a35091940914dc84
Noonanav <averynoonan@gmail.com>
2017-02-04 13:48:25 -0500

    target site match

--- plasmid_mod.py
+++ plasmid_mod.py
@@ -1,7 +1,26 @@
 import json
+import regex
+from Bio import SeqIO
+from Bio import SearchIO
+from Bio import Entrez
+from Bio.Seq import Seq
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+from Bio.Alphabet import generic_dna
 
 with open('enz_list.json') as data_file:
 	enz_list = json.load(data_file)
+
+#OPEN SEQUENCE FILE
+for record in SeqIO.parse("pCYAko.fasta","fasta"):
+    plasmid = record
+
+seq = plasmid.seq
+comseq = seq.complement()
+
+full_seq = seq + comseq
+full_seq_str = str(full_seq).upper()
 
 # TARGET CODE CHANGE
 
@@ -25,9 +44,12 @@ for i in init_site:
 	i = i.replace('H', '[ACT]')
 	i = i.replace('V', '[ACG]')
 	i = i.replace('N', '[ACGT]')
-	print i
-	# for x in i:
-	# 	if 'A' in x:
-	# 		x == '2'
-	# 		print i
-# print init_site
+	i = i.replace('^', '')
+	target_site.append(i.replace("\r", ""))
+
+match_str = "(" + ")|(".join(target_site) + ")"
+
+cut_site = regex.finditer(match_str, full_seq_str)
+
+for i in cut_site:
+	print i.group(0)
