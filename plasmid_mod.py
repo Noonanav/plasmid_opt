abf480e297f49f0f5099212500628bfccb5b2768
Avery Noonan <averynoonan@gmail.com>
2017-02-06 16:50:30 -0500

    rm site in orf

--- plasmid_mod.py
+++ plasmid_mod.py
@@ -22,6 +22,61 @@ comseq = seq.complement()
 full_seq = seq + comseq
 full_seq_str = str(full_seq).upper()
 
+# LOCATE ORFs
+stop_codon_list = []
+stop_codon = regex.finditer(r'(?!TGG)(T[AG][AG])', full_seq_str, overlapped=True)
+for match in stop_codon:
+    stop_codon_list.append(match)
+
+frame1 = []
+frame2 = []
+frame3 = []
+
+plasmid_range = range(len(full_seq_str) / 3)
+for p in plasmid_range:
+    frame1.append(0 + (p * 3))
+    frame2.append(1 + (p * 3))
+    frame3.append(2 + (p * 3))
+
+f1_codon = []
+f2_codon = []
+f3_codon = []
+
+for stop in stop_codon_list:
+    if stop.start() in frame1:
+        f1_codon.append(stop)
+    if stop.start() in frame2:
+        f2_codon.append(stop)
+    if stop.start() in frame3:
+        f3_codon.append(stop)
+
+orf_list = []
+
+for i in range(len(f1_codon) - 1):
+    orf = (f1_codon[i+1].start() - f1_codon[i].start())
+    if orf >= 300:
+        o = [f1_codon[i],orf]
+        orf_list.append(o)
+
+for i in range(len(f2_codon) - 1):
+    orf = (f2_codon[i+1].start() - f2_codon[i].start())
+    if orf >= 300:
+        o = [f2_codon[i],orf]
+        orf_list.append(o)
+
+for i in range(len(f3_codon) - 1):
+    orf = (f3_codon[i+1].start() - f3_codon[i].start())
+    if orf >= 300:
+        o = [f3_codon[i],orf]
+        orf_list.append(o)
+
+orfs = []
+for orf in orf_list:
+    o = full_seq_str[orf[0].end() : (orf[0].end() + orf[1])]
+    o_seq = Seq(o)
+    orfs1 = [orf[0].end()+1, orf[1], o_seq, o_seq.translate(table=11)]
+    orfs.append(orfs1)
+
 # TARGET CODE CHANGE
 
 init_site = []
@@ -33,10 +88,11 @@ meth_sites = []
 re_sites = []
 nick_sites = []
 homing = []
+re_loc = []
 
 
 for i in enz_list:
-	if 'Arthrospira platensis' in i[0]:
+	if 'Synechocystis species PCC 6803' in i[0]:
 		if '?' not in i[2]:
         		init_site.append(i)
 
@@ -64,7 +120,7 @@ for i in target_enz:
 for t in target_site:
 	for i in target_enz:
 		if i[2] in str(t.re):
-			rm_hit = [i[1], i[3], t.group(0), t.start()]
+			rm_hit = [i[1], i[3], t.group(0), t.span()]
 			rm_site.append(rm_hit)
 
 for rm in rm_site:
@@ -77,6 +133,64 @@ for rm in rm_site:
 	if 'Homing' in rm[1]:
 		homing.append(rm)
 
+re_loc = []
+meth_loc = []
+nick_loc = []
+hom_loc = []
+
+for re in re_sites:
+	re_loc.append(re[3])
+re_loc1 = list(set(re_loc))
+for met in meth_sites:
+	meth_loc.append(met[3])
+meth_loc1 = list(set(meth_loc))
+for n in nick_sites:
+	nick_loc.append(n[3])
+nick_loc1 = list(set(nick_loc))
+for h in homing:
+	hom_loc.append(h[3])
+hom_loc1 = list(set(hom_loc))
+
+# RM SITE IN ORF
+re_orf = []
+meth_orf = []
+nick_orf = []
+hom_orf = []
+
+for orf in orfs:
+	for i in range((orf[0]-1), (orf[0]-1)+orf[1]):
+		for re in re_loc1:
+			if re[0] == i:
+				re_o = [re, orf]
+				re_orf.append(re_o)
+		for meth in meth_loc1:
+			if meth[0] == i:
+				meth_o = [meth, orf]
+				meth_orf.append(meth_o)
+		for nick in nick_loc1:
+			if nick[0] == i:
+				n_o = [nick, orf]
+				nick_orf.append(n_o)
+		for h in hom_loc1:
+			if h[0] == i:
+				h_o = [h, orf]
+				hom_orf.append(h_o)
+
+# for n in re_orf:
+# 	print n
+# for meth in meth_loc1:
+# 	print meth
+
+# for orf in orfs:
+# 	print orf
+
+# for re in re_loc:
+# 	print re
+# print len(re_loc)
+# print len(re_loc1)
+#
+# print len(meth_loc)
+# print len(meth_loc1)
 
 
 # print len(target_site)
--- rebase.py
+++ rebase.py
@@ -2,25 +2,25 @@ import re
 import regex
 import json
 
-# f = open('rebase.txt')
-# file = (f.read())
-#
-# enz_list = []
-#
-# name = regex.finditer(r'<1>.*', file)
-#
-# for i in name:
-#     org = regex.search(r'<3>.*', file[i.end():])
-#     site = regex.search(r'<5>.*', file[i.end():])
-#     typ = regex.search(r'<8>.*', file[i.end():])
-#     enzyme = [org.group(0)[3:], i.group(0)[3:], site.group(0)[3:], typ.group(0)[3:]]
-#     enz_list.append(enzyme)
-#
-# open('./enz_list.json', 'w').write(json.dumps(enz_list))
-#
-with open('enz_list.json') as data_file:
-	enz_list = json.load(data_file)
+f = open('rebase.txt')
+file = (f.read())
+
+enz_list = []
+
+name = regex.finditer(r'<1>.*', file)
+
+for i in name:
+    org = regex.search(r'<3>.*', file[i.end():])
+    site = regex.search(r'<5>.[^(]*', file[i.end():])
+    typ = regex.search(r'<8>.*', file[i.end():])
+    enzyme = [org.group(0)[3:], i.group(0)[3:], site.group(0)[3:], typ.group(0)[3:]]
+    enz_list.append(enzyme)
+
+open('./enz_list1.json', 'w').write(json.dumps(enz_list))
 # #
-for i in enz_list:
-    if 'NIES-39' in i[0]:
-        print i[2]
+# with open('enz_list1.json') as data_file:
+# 	enz_list = json.load(data_file)
+# # #
+# for i in enz_list:
+#     if 'Synechocystis' in i[0]:
+#         print i[2]
