88064db41bc67550d6f6a38bdf148815d64a85c8
Noonanav <averynoonan@gmail.com>
2017-02-11 12:05:18 -0500

    rebase original

--- rebase.py
+++ rebase.py
@@ -11,16 +11,11 @@ name = regex.finditer(r'<1>.*', file)
 
 for i in name:
     org = regex.search(r'<3>.*', file[i.end():])
-    site = regex.search(r'<5>.[^(]*', file[i.end():])
+    site = regex.search(r'<5>.*', file[i.end():])
     typ = regex.search(r'<8>.*', file[i.end():])
     enzyme = [org.group(0)[3:], i.group(0)[3:], site.group(0)[3:], typ.group(0)[3:]]
     enz_list.append(enzyme)
 
-open('./enz_list1.json', 'w').write(json.dumps(enz_list))
-# #
-# with open('enz_list1.json') as data_file:
-# 	enz_list = json.load(data_file)
+# open('./enz_list1.json', 'w').write(json.dumps(enz_list))
 # # #
-# for i in enz_list:
-#     if 'Synechocystis' in i[0]:
-#         print i[2]
+# with open('enz_lis/]
