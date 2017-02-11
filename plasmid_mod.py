4a221aeab1100e1992d1c2d45eb15912ae490159
Noonanav <averynoonan@gmail.com>
2017-02-11 12:04:32 -0500

    ORF Insert modification

--- plasmid_mod.py
+++ plasmid_mod.py
@@ -1,5 +1,7 @@
 import json
 import regex
+import string
+import random
 from Bio import SeqIO
 from Bio import SearchIO
 from Bio import Entrez
@@ -133,6 +135,7 @@ for t in target_site:
 	for i in target_enz:
 		if i[2] in str(t.re):
 			rm_hit = [i[1], i[3], t.group(0), t.start(), t.end()]
+			# rm_hit = [Enzyme name, Enzyme type, target sequence, target start, target end]
 			rm_site.append(rm_hit)
 
 for rm in rm_site:
@@ -152,6 +155,7 @@ for orf in orfs:
 			if rm == i:
 				rm_o = [rm, orf]
 				rm_orf.append(rm_o)
+				# rm_orf = [rm target site, ORF]
 
 # RM SITE IN NON-CODING PLASMID
 orf_bp = list(set(orf_ranges))
@@ -162,27 +166,49 @@ for nc in non_coding:
 	for rm in rm_loc1:
 		if rm == nc:
 			rm_nonc.append(rm)
-# print rm_nonc
-# print rm_orf
-# print len(rm_orf)
-# print len(rm_nonc)
-# print type(non_coding)
 
 # IN-FRAME RM SEQUENCE/LOCATION
 site = []
 for n in rm_orf:
 	x = (n[0])-(n[1][0])
-	for i in range(3):
+	for i in range(1,4):
 		if (x + i) % 3 == 0:
 			seq = n[1][2][(x-(3-i)):((x-(3-i))+15)]
 			aa = n[1][3][((x-(3-i))/3):((x-(3-i))/3)+5]
 			for rm in rm_site:
 				if n[0] == rm[3]:
 					site1 = [rm[0], rm[1], rm[2], seq, n[0]-(3-i)]
+					# site1 = [Enzyme name, Enzyme type, Recognition bp, In-frame bp, In-frame target loc]
 					site.append(site1)
+
+# SEQUENCE MODIFICATION
+ran_ins = []
+mod_inserts = []
+for s in site:
+	random_seq = ''.join(random.SystemRandom().choice('ACTG') for _ in range(len(s[2])))
+	random_ins = str(s[3]).replace(s[2], random_seq)
+	if Seq(random_ins).translate() == s[3].translate():
+		mod_insert = [random_ins, s[3], s[2], s[4]]
+		mod_inserts.append(mod_insert)
+	# else:
+for mod in mod_inserts:
+	print mod[0]
+	print mod[1]
+	print Seq(mod[0]).translate()
+	print mod[1].translate()
+
+
+	# ran_ins.append(random_ins)
+# print ran_ins[1]
+# print site[1]
+# j = ''.join(random.SystemRandom().choice('ACTG') for _ in range(4))
+# print j
 			# print seq.translate()
-			# print aa
-# print site
+			# print a
+
+# print site[0]
+# print rm_orf[0]
+# print rm_site[0]
 # for s in site:
 # 	print [s[0], s[2], s[3].translate(), s[4]]
 # 	print s[3].complement()
@@ -191,32 +217,40 @@ for n in rm_orf:
 	# 	print 'fuck ya'
 	# else:
 	# 	print 'fuck no'
-# for meth in meth_loc1:
-# 	print meth
 
-# for orf in orfs:
-# 	print orf
-
-# for re in re_loc:
-# 	print re
-# print len(re_loc)
-# print len(re_loc1)
+# SEQUENCE MODIFICATION
+# for rm in rm_site:
+# 	full_seq_str = full_seq_str.replace(full_seq_str[rm[3]:rm[4]], 'FUCKYA')
+# 	# print rm[0]
+# 	# print full_seq_str[rm[3]:rm[0][4]]
+# 	# print rm[0][3]
+# 	# print rm[0][4]
+# 	# print full_seq_str[rm[4]:rm[5]]
+# 	# full_seq_str = full_seq_str.replace(full_seq_str[rm[0][3]:rm[0][4]], 'FUCKYA', maxreplace)
+# # print rm[0]
+# # print full_seq_str[rm[0][3]:rm[0][4]]
+# # print full_seq_str[rm_site[0][3]:rm_site[0][4]]
+# # full_seq_str = full_seq_str.replace(full_seq_str[rm_site[0][3]:rm_site[0][4]], 'FUCKYA')
 #
-# print len(meth_loc)
-# print len(meth_loc1)
+# # print rm_site[0][4]
+# print full_seq_str
 
-
-# print len(target_site)
-# print len(rm_site)
-
-
-
-# for t in target_enz:
-# 	if t[2] in i in target_site:
-# 		print str(i.re)
-# # print target_site
+# PLASMID DIAGRAM
+# gd_diagram = GenomeDiagram.Diagram("pCYAko")
+# gd_track_for_features = gd_diagram.new_track(1, name="ORFs")
+# gd_feature_set = gd_track_for_features.new_set()
 #
-# # print enz_list
+# for orfs in plasmid.features:
+#     if orfs.type == "ORF":
+#         color = colors.blue
+#     else:
+#         color = colors.lightblue
+#     gd_feature_set.add_feature(orfs, color=color, label=True)
 #
-# for i in target_site:
-# 	print i.re
+# gd_diagram.draw(format="linear", orientation="landscape", pagesize='A4',
+#                 fragments=4, start=0, end=len(full_seq_str))
+# gd_diagram.write("plasmid_linear.pdf", "PDF")
+
+# gd_diagram.draw(format="circular", circular=True, pagesize=(20*cm,20*cm),
+#                 start=0, end=len(plasmid), circle_core=0.7)
+# gd_diagram.write("plasmid_circular.pdf", "PDF")
