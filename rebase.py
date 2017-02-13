120bfe50a5f9b2c4841639753ea0ecd95a6c1339
Noonanav <averynoonan@gmail.com>
2017-02-12 23:38:41 -0500

    rebase () bug fix

--- rebase.py
+++ rebase.py
@@ -11,11 +11,9 @@ name = regex.finditer(r'<1>.*', file)
 
 for i in name:
     org = regex.search(r'<3>.*', file[i.end():])
-    site = regex.search(r'<5>.*', file[i.end():])
+    site = regex.search(r'<5>[A-Z\?\^]*', file[i.end():])
     typ = regex.search(r'<8>.*', file[i.end():])
     enzyme = [org.group(0)[3:], i.group(0)[3:], site.group(0)[3:], typ.group(0)[3:]]
     enz_list.append(enzyme)
 
-# open('./enz_list1.json', 'w').write(json.dumps(enz_list))
-# # #
-# with open('enz_lis/]
+open('./enz_list.json', 'w').write(json.dumps(enz_list))
